35a8c3e31732d84cee020b1a97516f0dd360c9ee
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-18 01:49:30 +0700

    keep clean for simple script

--- kub.ex
+++ kub.ex
@@ -4,8 +4,8 @@
 # of equation (K+U+B)^3=KUB
 # where digits is different
 
-import Enum, only: [map: 2, filter: 2, each: 2, sum: 1, uniq: 1]
-import Integer, only: [digits: 1, pow: 2]
+import Enum
+import Integer
 
 check = fn {v, kub} ->
   length(v) == 3 and pow(sum(v), 3) == kub
