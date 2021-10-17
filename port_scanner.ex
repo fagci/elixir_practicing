a650b61dd3c8501e8659ee3901f4006db9951738
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-17 13:21:15 +0700

    refactor

--- port_scanner.ex
+++ port_scanner.ex
@@ -6,17 +6,17 @@ check = fn port ->
   case :gen_tcp.connect(ip, port, [], 2000) do
     {:ok, s} ->
       :gen_tcp.close(s)
-      {true, port}
+      port
 
     _ ->
-      {false, port}
+      nil
   end
 end
 
 open_ports =
   ports
   |> Task.async_stream(check, max_concurrency: 256)
-  |> Enum.filter(fn {_, {open, _}} -> open end)
-  |> Enum.map(fn {_, {_, port}} -> port end)
+  |> Enum.map(fn {_, port} -> port end)
+  |> Enum.reject(&is_nil/1)
 
 IO.inspect(open_ports)
