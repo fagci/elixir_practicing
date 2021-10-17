1b9e0cb3946a8cf8f8b5bf04a6634b8b7ed27ab8
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-18 01:27:00 +0700

    cleaner

--- kub.ex
+++ kub.ex
@@ -4,12 +4,16 @@
 # of equation (K+U+B)^3=KUB
 # where digits is different
 
-102..987
-|> Enum.map(&({Enum.uniq(Integer.digits(&1)), &1}))
-|> Enum.filter(fn {v, kub} ->
+check = fn {v, kub} ->
   length(v) == 3 and Integer.pow(Enum.sum(v), 3) == kub
-end)
-|> Enum.map(fn {[k, u, b], kub} ->
+end
+
+format = fn {[k, u, b], kub} ->
   "(#{k}+#{u}+#{b})^3=#{kub}"
-end)
+end
+
+102..987
+|> Enum.map(&{Enum.uniq(Integer.digits(&1)), &1})
+|> Enum.filter(check)
+|> Enum.map(format)
 |> Enum.each(&IO.puts/1)
