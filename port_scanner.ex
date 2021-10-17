14d4c4b0a9ff774cd08f67d78a88ffb506962f0f
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-17 12:26:25 +0700

    port scanner

---
+++ port_scanner.ex
@@ -0,0 +1,23 @@
+#! /usr/bin/env elixir
+ip = String.to_charlist("192.168.0.250")
+ports = 1..1024
+
+open_ports =
+  ports
+  |> Task.async_stream(
+    fn port ->
+      case :gen_tcp.connect(ip, port, [], 2000) do
+        {:ok, s} ->
+          :gen_tcp.close(s)
+          {true, port}
+
+        _ ->
+          {false, port}
+      end
+    end,
+    max_concurrency: 256
+  )
+  |> Enum.filter(fn {_, {open, _}} -> open end)
+  |> Enum.map(fn {_, {_, port}} -> port end)
+
+IO.inspect(open_ports)
